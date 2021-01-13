61ff5000b94760b7e32d47be16bb04e9a0666bbb
Ramon Snellink <r.snellink@gmail.com>
2021-01-13 09:46:20 +0100

    added first ACF block

--- src/JackpineSite.php
+++ src/JackpineSite.php
@@ -55,14 +55,65 @@ class JackpineSite extends Site {
         add_action( 'init', [ $this, 'add_custom_taxonomies' ] );
         add_action( 'init', [ $this, 'add_custom_post_types' ] );
         add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_wpack_scripts' ] );
-    }
+        add_action( 'acf/init', [$this, 'my_acf_init' ] );
 
+        
+    }
     /**
      * Register our filters.
      */
     public function add_filters() {
         add_filter( 'timber/context', [ $this, 'add_to_context' ] );
         add_filter( 'timber/twig', [ $this, 'add_to_twig' ] );
+        // add_filter('timber/acf-gutenberg-blocks-templates', function () {
+        //     return ['assets/templates/blocks'];
+        //   });
+    }
+
+
+       /**
+     * Register ACF Blocks
+     */
+    public function my_acf_init() {
+        // Bail out if function doesn’t exist.
+        if ( ! function_exists( 'acf_register_block' ) ) {
+            return;
+        }
+    
+        // Register a new block.
+        acf_register_block( array(
+            'name'            => 'example_block',
+            'title'           => __( 'Example Block', 'jackpine' ),
+            'description'     => __( 'A custom example block.', 'jackpine' ),
+            'render_callback' => [$this, 'my_acf_block_render_callback'],
+            'category'        => 'formatting',
+            'icon'            => 'admin-comments',
+            'keywords'        => array( 'example' ),
+        ) );
+    }
+
+
+    /**
+ *  This is the callback that displays the block.
+ *
+ * @param   array  $block      The block settings and attributes.
+ * @param   string $content    The block content (emtpy string).
+ * @param   bool   $is_preview True during AJAX preview.
+ */
+    public function my_acf_block_render_callback( $block, $content = '', $is_preview = false ) {
+        $context = Timber::context();
+
+        // Store block values.
+        $context['block'] = $block;
+
+        // Store field values.
+        $context['fields'] = get_fields();
+
+        // Store $is_preview value.
+        $context['is_preview'] = $is_preview;
+
+        // Render the block.
+        Timber::render( 'blocks/example-block.twig', $context );
     }
 
     /**
--- wpackio.project.js
+++ wpackio.project.js
@@ -1,49 +1,50 @@
 const packageJson = require('./package.json')
 
 module.exports = {
-    appName: 'jackpine',
-    type: 'theme',
-    slug: 'jackpine',
-    bannerConfig: {
-        name: packageJson.name,
-        author: packageJson.author,
-        license: packageJson.license,
-        link: packageJson.homepage,
-        version: packageJson.version,
-        copyrightText: `This software is released under the ${packageJson.license} License (https://opensource.org/licenses/${packageJson.license})`,
-        credit: true,
-    },
-    files: [
-        {
-            name: 'app',
-            entry: {
-                main: ['./assets/js/app.js', './assets/sass/app.scss'],
-            },
-            webpackConfig: undefined,
-        },
-    ],
-    outputPath: 'dist',
-    hasReact: false,
-    disableReactRefresh: false,
-    hasSass: true,
-    hasLess: false,
-    hasFlow: false,
-    externals: {},
-    alias: undefined,
-    errorOverlay: true,
-    optimizeSplitChunks: true,
-    watch: [
-        'assets/templates/**/*.twig',
-        'src/**/*.php',
-        'theme/**/*.php',
-    ],
-    packageFiles: [
-        'dist/**',
-        'assets/templates/**',
-        'theme/**',
-        'vendor/**',
-        '*.md',
-        'LICENSE',
-    ],
-    packageDirPath: 'package',
+	appName: 'jackpine',
+	type: 'theme',
+	slug: 'jackpine',
+	bannerConfig: {
+		name: packageJson.name,
+		author: packageJson.author,
+		license: packageJson.license,
+		link: packageJson.homepage,
+		version: packageJson.version,
+		copyrightText: `This software is released under the ${packageJson.license} License (https://opensource.org/licenses/${packageJson.license})`,
+		credit: true
+	},
+	files: [
+		{
+			name: 'app',
+			entry: {
+				main: ['./assets/js/app.js', './assets/sass/app.scss']
+			},
+			webpackConfig: undefined
+		}
+	],
+	outputPath: 'dist',
+	hasReact: false,
+	disableReactRefresh: false,
+	hasSass: true,
+	hasLess: false,
+	hasFlow: false,
+	externals: {},
+	alias: undefined,
+	errorOverlay: true,
+	optimizeSplitChunks: true,
+	watch: [
+		'assets/templates/**/*.twig',
+		'src/**/*.php',
+		'theme/**/*.php',
+		'views/blocks/*.twig'
+	],
+	packageFiles: [
+		'dist/**',
+		'assets/templates/**',
+		'theme/**',
+		'vendor/**',
+		'*.md',
+		'LICENSE'
+	],
+	packageDirPath: 'package'
 }
